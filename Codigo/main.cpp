a3af2ecb66ce7d6769adb5b73c660b0ef68f9fef
chicho377 <morafabricio86@gmail.com>
2024-07-18 12:17:42 -0600

    Agregando funciones de utilidad (dibujarCuadrado, limpiarPantalla)

--- Codigo/main.cpp
+++ Codigo/main.cpp
@@ -46,6 +46,34 @@ void setColor(int color) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
+// dibuja un cuadro en la consola en las coordenadas (x1, y1) y (x2, y2)
+void dibujarCuadrado(int x1, int y1, int x2, int y2) {
+    for (int x = x1; x <= x2; x++) {
+        gotoxy(x, y1);
+        cout << (char)205;
+        gotoxy(x, y2);
+        cout << (char)205;
+    }
+    for (int y = y1; y <= y2; y++) {
+        gotoxy(x1, y);
+        cout << (char)186;
+        gotoxy(x2, y);
+        cout << (char)186;
+    }
+    gotoxy(x1, y1);
+    cout << (char)201;
+    gotoxy(x2, y1);
+    cout << (char)187;
+    gotoxy(x1, y2);
+    cout << (char)200;
+    gotoxy(x2, y2);
+    cout << (char)188;
+}
+
+// limpia la pantalla de la consola
+void limpiarPantalla() {
+    system("cls");
+}
 
 int main(int argc, char** argv) {
 	return 0;

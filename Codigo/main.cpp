e08ba647a7698f5d3417589535d8cb7b19007afb
chicho377 <morafabricio86@gmail.com>
2024-07-18 11:31:48 -0600

    Agregando variables globales y prototipos de funciones

--- Codigo/main.cpp
+++ Codigo/main.cpp
@@ -3,6 +3,36 @@
 #include <iomanip>
 #include <windows.h>
 
+using namespace std;
+
+// variables globales
+
+const int MAX_CANDIDATOS = 100; // Numero maximo de candidatos
+
+struct Candidato {
+    string nombre;
+    string partido;
+    string plataforma;
+    string domicilio;
+    string fechaNacimiento;
+    string correoElectronico;
+};
+
+Candidato candidatos[MAX_CANDIDATOS];
+int votos[MAX_CANDIDATOS];
+int numCandidatos = 0;
+
+// prototipos de funciones
+void gotoxy(int x, int y);
+void setColor(int color);
+void dibujarCuadrado(int x1, int y1, int x2, int y2);
+void limpiarPantalla();
+void showMenu();
+void agregarCandidato();
+void registrarVoto();
+void mostrarResultados();
+void mostrarCandidatos();
+
 int main(int argc, char** argv) {
 	return 0;
 }
